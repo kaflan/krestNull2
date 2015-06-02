085443ba201d96757645b0d9af55929d7b6596b2
kaflan <kaf_90@mail.ru>
2015-06-03 02:30:45 +0300

    save input value in storage

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 /* global getWinner */
-var data = localStorage.getItem('data');
-if (data) {
-  game = JSON.parse(data);
+var game = {
+  position: [],
+  size: null
+}; // local storage
+var loadState = localStorage.getItem('game');
+if (loadState) {
+  game = JSON.parse(loadState);
 }
 window.addEventListener('load', function load() {
   'use strict';
-  var state = {game: []}; //local storage
   var cellAll; // у массіві всі наші класси целл
   var nextClass;// хранить класс правильний по кліку фраєра змінюєця поточний клас сім матерів бабі в цицьку
   var i;
   var j;
   var winner;
   var inputVal;
-  var size;
+  var size = game.size;
   var field = document.querySelector('.field');
   var winnerEl = document.querySelector('.winner-message');
   var startNewGame = document.querySelector('.startNewGame');
@@ -63,18 +66,22 @@ window.addEventListener('load', function load() {
     winnerEl.innerHTML = '';
     field.addEventListener('click', clickEvent);
   }; // start to new game click
-  var createFields = function (e) {
-    e.preventDefault();
+  var createFields = function () {
+    //if(game.size[0]) {
+    //  size = +game.size[0];
+    //}
     localStorage.clear();
     inputVal = count.value;
     size = +inputVal;
-    field.innerHTML = "";
+    field.innerHTML = '';
     if (isNaN(size) || size < 5 || size > 15) {
       errMessage.innerHTML = 'Вы ввели некорректное число';
       return;
     }
     mainGame.style.display = 'inline-block';
     startGame.style.display = 'none';
+    console.log(size);
+    game.size = size;
     for (i = 0; i < size; i++) {
       var divRow = createDiv();
       addRowClass(divRow);
@@ -85,20 +92,16 @@ window.addEventListener('load', function load() {
         divRow.appendChild(divCell);
       }
     }
-    save();
+    saveState();
   }; // create fields
-  var num = function(){
-    inputVal = count.value;
-    size = +inputVal;
-    state.game.push[size];
+  var saveState = function () {
+    localStorage.setItem('game', JSON.stringify(game));
   };
-  var save = function(){
-    for(i = 0; i < state.game.length; i++){
-      state.game.push[i];
-      num();
-    }
-    localStorage.setItem('data', JSON.stringify(state));
-  };
+  if (game.size){
+    count.value = game.size;
+    return createFields();
+  }
+  console.log(game.size);
   startNewGame.addEventListener('click', clickNewGame);
   generateField.addEventListener('click', createFields);
   count.addEventListener('keyup', function (event) {
